36c0fd82356364e66d18cdf52217a339c4bfcfd2
Volodymyr Yakovenko <vovayakovenko2005@gmail.com>
2023-05-28 00:13:13 +0300

    54 lesson

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({ 
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron/right.png"></button>',
+        responsive: [
+            {
+                breakpoing: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+  });
