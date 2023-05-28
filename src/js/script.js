f620e86deccda868fd483cbee68c632e7419ee8a
Volodymyr Yakovenko <vovayakovenko2005@gmail.com>
2023-05-28 14:03:12 +0300

    55 commit new slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,36 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({ 
-        speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron/right.png"></button>',
-        responsive: [
-            {
-                breakpoing: 768,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({ 
+//         speed: 1200,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron/left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron/right.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoing: 768,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//     });
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    speed: 1200,
+    autoplay: 2500,
+    autoplayButtonOutput: false,
+    autoplayHoverPause: true,
   });
+
+document.querySelector('.prev').addEventListener('click', function(){
+  slider.goTo('prev')
+});
+document.querySelector('.next').addEventListener('click', function(){
+  slider.goTo('next')
+});
