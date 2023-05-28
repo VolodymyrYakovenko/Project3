468362ae660bd616d2e2aa98c2ae521a9a6a8a6b
Volodymyr Yakovenko <vovayakovenko2005@gmail.com>
2023-05-29 00:38:34 +0300

    adding catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,24 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({ 
-//         speed: 1200,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron/left.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron/right.png"></button>',
-//         responsive: [
-//             {
-//                 breakpoing: 768,
-//                 settings: {
-//                     dots: true,
-//                     arrows: false
-//                 }
-//             }
-//         ]
-//     });
-//   });
+$(document).ready(function(){
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+  
+  function toggleSlide(item) {
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    })
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back')
+
+});
 
 const slider = tns({
     container: '.carousel__inner',
@@ -34,3 +39,4 @@ document.querySelector('.prev').addEventListener('click', function(){
 document.querySelector('.next').addEventListener('click', function(){
   slider.goTo('next')
 });
+
