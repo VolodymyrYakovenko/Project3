22a6295bf4e45b890f8ef88acfedcef2c47a8d36
Volodymyr Yakovenko <vovayakovenko2005@gmail.com>
2023-06-04 20:15:52 +0300

    verified validation

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,19 @@ $(document).ready(function(){
     });
     return false;
   });
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1600){
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+  $("a[href='#up'").click(function(){
+    const _href = $(this).attr("href");
+    $("html").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+  new WOW().init();
 });
 
 const slider = tns({
