364a32f563c866b27c411bfedb3b1d8f3ef00693
Volodymyr Yakovenko <vovayakovenko2005@gmail.com>
2023-06-04 17:14:32 +0300

    adding a bit of backend

--- src/js/script.js
+++ src/js/script.js
@@ -57,7 +57,26 @@ $(document).ready(function(){
   validateForms('#consultation-form');
   validateForms('#consultation form');
   validateForms('#order form');
+
   $('input[name=phone]').mask("+38 (999) 999-9999");
+
+  $('form').submit(function(e){
+    e.preventDefault();
+    if(!$(this).valid()){
+      return; 
+    }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function(){
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      $('form').trigger('reset');
+    });
+    return false;
+  });
 });
 
 const slider = tns({
